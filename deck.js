8bd016b725257f8656560dc3536c0bb083b5a353
ssembara <sembara9090@gmail.com>
2020-05-30 16:13:40 +0700

    initial commit

---
+++ deck.js
@@ -0,0 +1,14 @@
+// import global stylesheet
+
+import { slides as title } from './slides/01-title.mdx'
+import { slides as introduction } from './slides/02.introduction.mdx'
+
+// import `slides` from your mdx files and spread into `slides` export
+// theme export for code-surfer
+export { nightOwl as theme } from 'code-surfer'
+
+// theme exports for mdx-deck since `theme` is used by code-surfer
+export const themes = []
+
+// Spread your slides here...
+export const slides = [...title, ...introduction]
