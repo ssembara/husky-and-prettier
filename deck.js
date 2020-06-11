152df0591d829a648bc0a67cb7fd69c7c3c92650
ssembara <sembara9090@gmail.com>
2020-06-11 18:50:28 +0700

    add content_1 v.1

--- deck.js
+++ deck.js
@@ -1,6 +1,7 @@
 // import `slides` from your mdx files and spread into `slides` export
 import { slides as title } from './slides/01-title.mdx'
 import { slides as introduction } from './slides/02-introduction.mdx'
+import { slides as content_1 } from './slides/03-content-1.mdx'
 import { duotoneDark } from 'code-surfer'
 
 
@@ -11,4 +12,4 @@ export { duotoneDark as theme } from 'code-surfer'
 export const themes = [duotoneDark]
 
 // Spread your slides here...
-export const slides = [...title, ...introduction]
+export const slides = [...title, ...introduction, ...content_1]
