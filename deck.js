4dcac623e692f654caa189b63a645f4a5f297bbb
ssembara <sembara9090@gmail.com>
2020-06-11 10:40:35 +0700

    cleanup

--- deck.js
+++ deck.js
@@ -1,14 +1,14 @@
-// import global stylesheet
-
+// import `slides` from your mdx files and spread into `slides` export
 import { slides as title } from './slides/01-title.mdx'
 import { slides as introduction } from './slides/02-introduction.mdx'
+import { duotoneDark } from 'code-surfer'
 
-// import `slides` from your mdx files and spread into `slides` export
+
 // theme export for code-surfer
 export { duotoneDark as theme } from 'code-surfer'
 
 // theme exports for mdx-deck since `theme` is used by code-surfer
-export const themes = [""]
+export const themes = [duotoneDark]
 
 // Spread your slides here...
 export const slides = [...title, ...introduction]
