a138148a6652443b1c00bd8382aea6f41a6a480e
ssembara <sembara9090@gmail.com>
2020-06-11 10:02:44 +0700

    add introduction deck

--- deck.js
+++ deck.js
@@ -5,10 +5,10 @@ import { slides as introduction } from './slides/02.introduction.mdx'
 
 // import `slides` from your mdx files and spread into `slides` export
 // theme export for code-surfer
-export { nightOwl as theme } from 'code-surfer'
+export { duotoneDark as theme } from 'code-surfer'
 
 // theme exports for mdx-deck since `theme` is used by code-surfer
-export const themes = []
+export const themes = [""]
 
 // Spread your slides here...
 export const slides = [...title, ...introduction]
